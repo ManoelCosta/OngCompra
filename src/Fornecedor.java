4884186a31ad5f03dacdd8398c81db2b6ebd5aa5
MANOEL SILVA COSTA <26003980@sempreunama.com.br>
2019-05-27 21:15:07 -0300

    Initial commit

---
+++ src/Fornecedor.java
@@ -0,0 +1,12 @@
+public class Fornecedor extends Usuario{
+
+
+    public Fornecedor(String usuario, String senha) {
+        super(usuario, senha);
+    }
+
+    public void castrarProduto(){
+        
+    }
+
+}
---
+++ src/Usuario.java
@@ -0,0 +1,43 @@
+public class Usuario {
+    private String nome;
+    private String identidade;
+    private String usuario;
+    private String senha;
+
+    public Usuario(String usuario, String senha) {
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdentidade() {
+        return identidade;
+    }
+
+    public void setIdentidade(String identidade) {
+        this.identidade = identidade;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
